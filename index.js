3f3b05208b215ef47e3b1a99b371b21cd0fba4e7
musin2 <muhsindaud75@gmail.com>
2024-07-12 13:09:49 +0300

    adds lab code

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    console.log(customerName);
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    console.log(bestCustomer);
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    console.log(bestCustomer);
+}
+
+const leastFavouriteCustomer = 'j'; 
+
+function changeLeastFavoriteCustomer() {
+    leastFavouriteCustomer = 'john';    
+}
+
+upperCaseCustomer();
+setBestCustomer();
+overWriteBestCustomer();
+changeLeastFavoriteCustomer();
+
+console.log(customerName);
+console.log(leastFavouriteCustomer);
